1af7f7fc9e20d92e4e4a622cddc6e798ae6992f0
Varian Gilrandy R.A <itachiu603@gmail.com>
2021-05-17 22:03:00 +0700

    Avatar KomentarList

--- src/Components/Apps/Detail/KomentarList.jsx
+++ src/Components/Apps/Detail/KomentarList.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { TextField, makeStyles, Grid, Avatar } from "@material-ui/core";
+import { TextField, makeStyles, Grid, Avatar, ListItem, List, ListItemAvatar, ListItemText, Typography } from "@material-ui/core";
 import { withRouter } from "react-router-dom";
 import { withFirebase } from "../../../Firebase";
 import { compose } from "recompose";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
+
 const ListKomentar = (props) => {
+    const classes = useStyles();
     const [KomenList, setKomenList] = useState([]);
+
 
     useEffect(() => {
         props.firebase.getKomentar(props.endpoint).on('value', snapshot => {
@@ -19,20 +32,46 @@ const ListKomentar = (props) => {
     }, [])
 
     return (
-        <div>
+
+        <List className={classes.root}>
             {KomenList.map((e, i) => (
-                <Grid container key={i} xs={12}
-                // className={classes.GridKomentar}
-                >
-                    <Grid item xs={12} >
-                        {e.email}
-                    </Grid>
-                    <Grid item xs={12} >
-                        {e.komen}
-                    </Grid>
-                </Grid>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt={e.email} src="/broken-image.jpg" >
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={
+                        <React.Fragment>
+                            <Typography
+                                component="span"
+                                variant="body2"
+                                className={classes.inline}
+                                color="textPrimary"
+                            >
+
+                            </Typography>
+                            {e.email}
+                        </React.Fragment>
+                    } secondary={e.komen} />
+                </ListItem>
+                // <div>
+                //     <Grid container key={i} xs={2}>
+                //         <Avatar alt={e.email} src="/broken-image.jpg" >
+                //         </Avatar>
+                //     </Grid>
+                //     <Grid container key={i} xs={10}
+                //     // className={classes.GridKomentar}
+                //     >
+                //         <Grid item xs={12} >
+                //             {e.email}
+                //         </Grid>
+                //         <Grid item xs={12} >
+                //             {e.komen}
+                //         </Grid>
+                //     </Grid>
+                // </div>
             ))}
-        </div>
+        </List>
     )
 }
 
