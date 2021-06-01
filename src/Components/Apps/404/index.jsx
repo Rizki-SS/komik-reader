402c35f1b60623c5a3c909474a28193faea129d3
Varian Gilrandy R.A <itachiu603@gmail.com>
2021-06-01 21:49:16 +0700

    notfound

---
+++ src/Components/Apps/404/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import img from '../../../Asset/Img/paimonshock.jpg'
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    paimon: {
+        width: "50%",
+        height: "50%",
+        marginTop: "50px",
+    },
+    root: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems: "center",
+    }
+
+}));
+
+const Notfound = () => {
+    const classes = useStyles()
+
+    return (
+        <div
+            className={classes.root}>
+            <img
+                className={classes.paimon}
+                src={img}>
+            </img>
+            <h1>Server Not Found :)</h1>
+        </div>
+
+    )
+}
+export default Notfound
